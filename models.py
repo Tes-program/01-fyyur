e88b6ddb22e81743365982fb9f8d4562b3de24d5
Tes-program <teslimodumuyiwa@gmail.com>
2022-06-03 14:43:01 +0100

    models and app completed

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
@@ -9,6 +15,12 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)), default={})
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='Venue', lazy=True)
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -23,5 +35,21 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+
+
+
+class Show(db.Model):
+    _tablename_ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.today())
+
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
